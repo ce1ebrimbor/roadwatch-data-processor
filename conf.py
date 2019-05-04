99d0f44bd71b1089de39cba8079b53936fbc98e5
Daniel <daniel.sasu.etu@univ-lille.fr>
2019-05-04 16:02:08 +0200

    Change version to v0.2.4

--- conf.py
+++ conf.py
@@ -27,9 +27,9 @@ copyright = '2019, Daniel SASU'
 author = 'Daniel SASU'
 
 # The short X.Y version
-version = '0.2.3'
+version = '0.2.4'
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
